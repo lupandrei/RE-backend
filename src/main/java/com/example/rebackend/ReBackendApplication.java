decceb2d1d15a542685a5690fdf740400115d066
Kaisser Thomas <kaisser.thomas@gmail.com>
2025-01-12 17:20:01 +0200

    first commit

---
+++ src/main/java/com/example/rebackend/ReBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.rebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rebackend/ReBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
